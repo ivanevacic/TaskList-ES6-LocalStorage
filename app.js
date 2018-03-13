7bd2b18f0f5924524c0f837dd268f78f2be62c5d
ivanevacic <ivanevacicweb@gmail.com>
2018-03-13 22:12:09 +0100

    Fixed code style and comments

--- app.js
+++ app.js
@@ -5,107 +5,131 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
-//  Call function that loads all listeners
-loadEventListeners();
 
-//  Load all event listeners
-function loadEventListeners() {
-    //  DOM Load Event
-    document.addEventListener('DOMContentLoaded', getTasks);    //  Event that executes when DOM loads
-    //  Listen for submit action on form variable and execute function addTask
-    form.addEventListener('submit', addTask);
-    //  Listen for click on element with .collection class and execute function removeTask
-    taskList.addEventListener('click', removeTask);
-    //  Listen for click on element with .clear-tasks class and execute function clearTasks
-    clearBtn.addEventListener('click', clearTasks);
-    //  Listen for keystrong on element with filter id and execute function filterTasks
-    filter.addEventListener('keyup', filterTasks);   
+
+
+/*----   EVENT LISTENERS FUNCTION    ----*/
+
+    //  Load all event listeners
+    function loadEventListeners() {
+        //  DOM Load Event
+        document.addEventListener('DOMContentLoaded', getTasks);    //  Event that executes when DOM loads
+        //  Listen for submit action on form variable and execute function addTask
+        form.addEventListener('submit', addTask);
+        //  Listen for click on element with .collection class and execute function removeTask
+        taskList.addEventListener('click', removeTask);
+        //  Listen for click on element with .clear-tasks class and execute function clearTasks
+        clearBtn.addEventListener('click', clearTasks);
+        //  Listen for keystrong on element with filter id and execute function filterTasks
+        filter.addEventListener('keyup', filterTasks);   
+    }
+    //  Call function that loads all listeners
+    loadEventListeners();
+
+/*-------------------------------------*/
+
+
+
+
+/*----  ADD TASK FUNCTIONS  ----*/
+
+    //  Add Task function
+    function addTask(e) {
+        //  If taskInput is empty
+        if(taskInput.value === ''){
+            //  Alert user that input is empty
+            alert('Can\'t add empty input!');
+            //  Stops function from executing further
+                //  Fixes bug when confirming alert would anyways create empty task
+                    return;
+        }
+        //  Create li element
+        const li = document.createElement('li');
+        //  Add css class to element
+        li.className = 'collection-item';
+        //  Create text node with input value and append it to li
+        li.appendChild(document.createTextNode(taskInput.value));
+        // Create new link element
+        const link = document.createElement('a');
+        link.className = 'delete-item secondary-content';
+        //  Add delete icon (x)
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        //  Append link to li
+        li.appendChild(link);
+        //  Append li to the ul
+        taskList.appendChild(li);
+        //  Store task in local storage
+        storeTaskInLocalStorage(taskInput.value);
+        //  Clear input
+        taskInput.value = '';
+        //  Prevent default behaviour(form submit)
+        e.preventDefault();
+    }
+
+    //  Store task in Local Storage function
+    function storeTaskInLocalStorage(task) {
+    //  Define variable to see if there are tasks in local storage
+        let tasks;
+        //  If there aren't any tasks 
+        if(localStorage.getItem('tasks') === null) {  
+            //  Set let tasks to empty array     
+            tasks = [];
+            }   else {
+            //  'Fill' let tasks with tasks from Local Storage
+                //  Local storage contains only strings,we need to parse it to JSON
+                    tasks = JSON.parse(localStorage.getItem('tasks'));
+            }
+        tasks = Array.from(tasks);
+        //  Push added task to the array
+        tasks.push(task);
+        //  Convert task to string and save into Local Storage
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
 }
+/*-------------------------------------*/
+
+
+
+
+/*----  SHOW TASKS FUNCTIONS  ----*/
 
 //  Get tasks from Local Storage
-function getTasks() {
-    let tasks;
-    if(localStorage.getItem('tasks') === null) {
-        tasks = [];
-    }   else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-    //  Local Storage returs 'tasks' as object -> change it to array
-    tasks = Array.from(tasks);
-    //  Loop trough tasks
-     tasks.forEach(function(task){
-        //  Create DOM elements
-            //  Create li element
-            const li = document.createElement('li');
-            //  Add css class to element
-            li.className = 'collection-item';
-            //  Create text node with input value and append it to li
-            li.appendChild(document.createTextNode(task));
-            // Create new link element
-            const link = document.createElement('a');
-            link.className = 'delete-item secondary-content';
-            //  Add delete icon (x)
-            link.innerHTML = '<i class="fa fa-remove"></i>';
-            //  Append link to li
-            li.appendChild(link);
-            //  Append li to the ul
-            taskList.appendChild(li);
-    }); 
-}
+        function getTasks() {
+            let tasks;
+            if(localStorage.getItem('tasks') === null) {
+                tasks = [];
+                }   else {
+                    tasks = JSON.parse(localStorage.getItem('tasks'));
+                }
+            //  Local Storage returs 'tasks' as object -> change it to array
+            tasks = Array.from(tasks);
+            //  Loop trough tasks
+            tasks.forEach(function(task){
+                //  Create DOM elements
+                    //  Create li element
+                    const li = document.createElement('li');
+                    //  Add css class to element
+                    li.className = 'collection-item';
+                    //  Create text node with input value and append it to li
+                    li.appendChild(document.createTextNode(task));
+                    // Create new link element
+                    const link = document.createElement('a');
+                    link.className = 'delete-item secondary-content';
+                    //  Add delete icon (x)
+                    link.innerHTML = '<i class="fa fa-remove"></i>';
+                    //  Append link to li
+                    li.appendChild(link);
+                    //  Append li to the ul
+                    taskList.appendChild(li);
+            }); 
+        }
+
+/*-------------------------------------*/
 
 
-//  Add Task function
-function addTask(e) {
-    //  If taskInput is empty
-    if(taskInput.value === ''){
-        //  Alert user that input is empty
-        alert('Can\'t add empty input!');
-        //  Stops function from executing further
-            //  Fixes bug when confirming alert would anyways create empty task
-                return;
-    }
-    //  Create li element
-    const li = document.createElement('li');
-    //  Add css class to element
-    li.className = 'collection-item';
-    //  Create text node with input value and append it to li
-    li.appendChild(document.createTextNode(taskInput.value));
-    // Create new link element
-    const link = document.createElement('a');
-    link.className = 'delete-item secondary-content';
-    //  Add delete icon (x)
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    //  Append link to li
-    li.appendChild(link);
-    //  Append li to the ul
-    taskList.appendChild(li);
-    //  Store task in local storage
-    storeTaskInLocalStorage(taskInput.value);
-    //  Clear input
-    taskInput.value = '';
-    //  Prevent default behaviour(form submit)
-    e.preventDefault();
-}
 
-//  Store task in Local Storage function
-function storeTaskInLocalStorage(task) {
-    //  Define variable to see if there are tasks in local storage
-    let tasks;
-    //  If there aren't any tasks 
-    if(localStorage.getItem('tasks') === null) {  
-        //  Set let tasks to empty array     
-        tasks = [];
-    }   else {
-        //  'Fill' let tasks with tasks from Local Storage
-        //  Local storage contains only strings,we need to parse it to JSON
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-    tasks = Array.from(tasks);
-    //  Push added task to the array
-    tasks.push(task);
-    //  Convert task to string and save into Local Storage
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+
+/*----  REMOVE TASKS FUNCTIONS  ----*/
 
 //  Remove Task function
     //  Remove specific task
@@ -141,6 +165,13 @@ function storeTaskInLocalStorage(task) {
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }
 
+/*-------------------------------------*/
+
+
+
+
+/*----  CLEAR TASKS FUNCTIONS  ----*/
+
 //  ClearTasks  function
     //  Deletes all tasks
     function clearTasks() {
@@ -160,26 +191,35 @@ function clearTasksFromLocalStorage() {
     localStorage.clear();
 }
 
+/*-------------------------------------*/
+
+
+
+
+/*----  CLEAR TASKS FUNCTIONS  ----*/
+
 //  FilterTasks function
     //  Filters trough tasks based on current input
-        function filterTasks(e) {
-            //   Value of whatever is being currently typed in,to lower case(so it can be matched correctly)
-            const text = e.target.value.toLowerCase(); 
-            //  Select all elements that have a class of .collection item
-                //  Loop trough them(we can use forEach,since querySelectorAll returns nodeList)
-            document.querySelectorAll('.collection-item').forEach(
-                function(task){ //  task->iterator in forEach
-                    //  Get value of first child
-                    const item = task.firstChild.textContent;
-                    //  If value of item is equal to the currently typed input
-                        //  If there is match =>it is == -1,we check for opposite,so != -1
-                            if(item.toLowerCase().indexOf(text) != -1) {
-                                //  Show text if true
-                                task.style.display = 'block'
-                            }   else {
-                                //  Don't show anything
-                                task.style.display = 'none';
-                            }
-                });
-        }
+    function filterTasks(e) {
+        //   Value of whatever is being currently typed in,to lower case(so it can be matched correctly)
+        const text = e.target.value.toLowerCase(); 
+        //  Select all elements that have a class of .collection item
+            //  Loop trough them(we can use forEach,since querySelectorAll returns nodeList)
+        document.querySelectorAll('.collection-item').forEach(
+            function(task){ //  task->iterator in forEach
+                //  Get value of first child
+                const item = task.firstChild.textContent;
+                //  If value of item is equal to the currently typed input
+                    //  If there is match =>it is == -1,we check for opposite,so != -1
+                        if(item.toLowerCase().indexOf(text) != -1) {
+                            //  Show text if true
+                            task.style.display = 'block'
+                        }   else {
+                            //  Don't show anything
+                            task.style.display = 'none';
+                        }
+            });
+    }
+
+/*-------------------------------------*/
 
